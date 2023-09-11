64264072a6d1249057a8243dd8ee3e73abcd22ee
Sahil Pingale <sahilpingale@gmail.com>
2023-09-12 01:48:40 +0530

    test

--- frontend/src/app/components/ProjectCard/page.tsx
+++ frontend/src/app/components/ProjectCard/page.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
-export default function ProjectCard({project}:any){
+export default async function ProjectCard({projectData}:any){
+  const project = await projectData
     return(
         <div className="portfolio-card">
         <div className="portfolio-text-box">
-          <p className="portfolio-title">{project?.title}</p>
+          <p className="portfolio-title">{project.title}</p>
           <p className="portfolio-description">
-            {project?.description}
+            {project.description}
           </p>
         </div>
         <div className="portfolio-button-area">
-          <Link href={`/projects/${project?.id}`}className="portfolio-btn">
+          <Link href={`/projects/${project.id}`}className="portfolio-btn">
             Details &rarr;
           </Link>
         </div>
