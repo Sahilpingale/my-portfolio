e4ba5d85bdd0625e19cfc324486779bba7f4e5a4
Sahil Pingale <sahilpingale@gmail.com>
2023-09-11 14:26:47 +0530

    use prisma instead of api

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,31 +3,22 @@ import Header from "./components/SectionHeader/page";
 import AboutMe from "./components/SectionAboutMe/page";
 import Skill from "./components/SectionSkills/page";
 import PortFolio from "./components/SectionPortfolio/page";
-import axios from "axios";
-
 
 export default async function Name() {
+const prisma = new PrismaClient()
+  const myProjects = await prisma.project.findMany()
+
   // const projects = async () => {
-  //   const response = await axios.get(
-  //     `https://my-portfolio-8n98fsowv-sahilpingale.vercel.app/api/projects/manage-project`
-  //   );
-  //   // console.log("texrr",response.data)
-  //   return response.data;
+  //   const url = `${process.env.DOMAIN}/api/projects/manage-project`
+  //   console.log(url)
+  //   const response = await fetch(url,{cache:"no-cache"})
+  //   return response.json()
   // };
-  const projects = async () => {
-    // const url = `${process.env.VERCEL_URL}/api/projects/manage-project`
-    const url = `${process.env.DOMAIN}/api/projects/manage-project`
-    console.log(url)
-    const response = await fetch(url,{cache:"no-cache"})
-    // console.log(response.json(),"ress")
-    return response.json()
-  };
-  const myProjects = await projects();
-  // console.log(myProjects,"reess")
+  // const myProjects = await projects();
 
   return (
     <div>
-      {myProjects.data?.map((p: any) => (
+      {myProjects.map((p: any) => (
         <div key={p.title}>{p.title}</div>
       ))}
       <Header />
