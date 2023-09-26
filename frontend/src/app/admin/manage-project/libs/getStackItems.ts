ea3cd01c703f6ae2db6e47c5582499852b6ff59a
Sahil Pingale <sahilpingale@gmail.com>
2023-09-27 01:07:33 +0530

    test

--- frontend/src/app/admin/manage-project/libs/getStackItems.ts
+++ frontend/src/app/admin/manage-project/libs/getStackItems.ts
@@ -1,5 +1,5 @@
 export default async function getStackItems(){
-    const response = await fetch(`${process.env.DOMAIN}/api/tech-stack/manage`,{cache:"no-store"})
+    const response = await fetch(`/api/tech-stack/manage`,{cache:"no-store"})
     const responseJSON = await response.json()
     if(!response.ok){
         throw new Error(responseJSON.message)
