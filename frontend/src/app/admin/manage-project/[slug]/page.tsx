c485f7c03a9bed89fe38ed8761b0466f9476cd9a
Sahil Pingale <sahilpingale@gmail.com>
2023-09-27 01:19:01 +0530

    test

--- frontend/src/app/admin/manage-project/[slug]/page.tsx
+++ frontend/src/app/admin/manage-project/[slug]/page.tsx
@@ -13,20 +13,20 @@ export default async function EditProject({
 
   const project = await getProjectDetails(projectId);
   // Fetch all tech stack items
-  const allStackItems = await getStackItems();
+  // const allStackItems = await getStackItems();
 
-  // Filter out Tech stack items and tools
-  const techStackItems = allStackItems.data.filter(
-    (item: IOption) => item.type === "TECHSTACK"
-  );
-  const toolItems = allStackItems.data.filter(
-    (item: IOption) => item.type === "TOOL"
-  );
+  // // Filter out Tech stack items and tools
+  // const techStackItems = allStackItems.data.filter(
+  //   (item: IOption) => item.type === "TECHSTACK"
+  // );
+  // const toolItems = allStackItems.data.filter(
+  //   (item: IOption) => item.type === "TOOL"
+  // );
   return (
     <div>
       <FormGroup
-        toolItems={toolItems}
-        techStackItems={techStackItems}
+        // toolItems={toolItems}
+        // techStackItems={techStackItems}
         projectData={project.data}
         id={projectId}
       />
